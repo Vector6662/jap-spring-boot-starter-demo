9db8cf5f668868c7f0806e6d19baebb6baa89bc3
Vector6662 <825266113@qq.com>
2021-09-25 23:00:00 +0800

    完善了文档，完成了测试

--- src/main/java/my/dong6662/japspringbootstarterdemo/controller/Oauth2Controller.java
+++ src/main/java/my/dong6662/japspringbootstarterdemo/controller/Oauth2Controller.java
@@ -1,40 +1,69 @@
 package my.dong6662.japspringbootstarterdemo.controller;
 
+import com.fujieid.jap.core.JapUser;
 import com.fujieid.jap.core.result.JapResponse;
+import com.fujieid.jap.core.store.JapUserStore;
 import com.fujieid.jap.spring.boot.starter.JapTemplate;
+import com.xkcoding.json.util.Kv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/oauth")
 public class Oauth2Controller {
     @Autowired
     JapTemplate japTemplate;
+    @Autowired
+    JapUserStore japUserStore;
 
-    // FIXME: 2021/9/12 测试通过了gitee平台的authorization-code和authorization-code授权方式
+    /**
+     * gitee AuthorizationCode授权方式
+     */
     @RequestMapping("/gitee/authorization-code")
     public JapResponse authorizationCode(){
         return japTemplate.opsForOauth2().authenticateByAuthorizationCode("gitee");
     }
 
+    /**
+     * gitee password授权方式
+     */
     @RequestMapping("/gitee/password")
     public JapResponse passwordGrantType(String username, String password) {
         return japTemplate.opsForOauth2().authenticateByPassword("gitee", username, password);
     }
 
+    @RequestMapping("/gitee/refresh-token")
+    public JapResponse refreshToken(HttpServletRequest request, HttpServletResponse response){
+        // 这是获取JapUser的一种方式
+        JapUser japUser = japUserStore.get(request, response);
+        // FIXME: 2021/9/25 gitee平台不会在授权信息中返回refresh_token
+        String refreshToken = ((Kv) japUser.getAdditional()).getString("refresh-token");
+        return japTemplate.opsForOauth2().refreshToken("gitee",refreshToken);
+    }
 
-
+    /**
+     * github AuthorizationCode授权方式
+     * @return
+     */
     @RequestMapping("/github/authorization-code")
     public JapResponse github(){
+        // FIXME: 2021/9/25 GitHub的授权方式似乎并不是严格的oauth2，查看：https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#response
+        //  会发现默认情况下返回的授权信息不是json格式，而是类似这样：access_token=XXX&token_type=bearer
         return japTemplate.opsForOauth2().authenticateByAuthorizationCode("github");
     }
 
 
-
+    /**
+     * 微博 AuthorizationCode授权方式
+     * @return
+     */
+    // FIXME: 2021/9/25 尚未测试
     @RequestMapping("/weibo/authorization-code")
-    public JapResponse weibo(String code){
+    public JapResponse weibo(){
         return japTemplate.opsForOauth2().authenticateByAuthorizationCode("weibo");
-
     }
 }
--- src/main/java/my/dong6662/japspringbootstarterdemo/controller/SocialController.java
+++ src/main/java/my/dong6662/japspringbootstarterdemo/controller/SocialController.java
@@ -1,11 +1,20 @@
 package my.dong6662.japspringbootstarterdemo.controller;
 
+import com.fujieid.jap.core.JapUser;
+import com.fujieid.jap.core.context.JapAuthentication;
 import com.fujieid.jap.core.result.JapResponse;
+import com.fujieid.jap.core.store.JapUserStore;
+import com.fujieid.jap.core.util.JapUtil;
 import com.fujieid.jap.spring.boot.starter.JapTemplate;
+import me.zhyd.oauth.model.AuthToken;
+import me.zhyd.oauth.model.AuthUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @RestController
@@ -13,11 +22,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class SocialController {
     @Autowired
     JapTemplate japTemplate;
+    @Autowired
+    JapUserStore japUserStore;
 
     @RequestMapping(method = RequestMethod.GET, path = "/gitee")
-    public JapResponse gitee() {
-        // FIXME: 2021/9/12 gitee不会返回state参数，即使访问gitee.com/oauth/authorize时带上了state参数。
-        //  https://gitee.com/oauth/authorize?response_type=code&client_id=228d103043840b9706f04ad165726a0079c7e0263bf7c11f1205b4054ff094a9&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fsocial%2Fgitee&state=3242vregv&scope=user_info
+    public JapResponse giteeCallback() {
         return japTemplate.opsForSocial().authenticate("gitee");
     }
+
+    @RequestMapping("/gitee/user-info")
+    public JapResponse userInfo(HttpServletRequest request, HttpServletResponse response){
+        // japUserStore实现类为SessionJapUserStore时，获取的是当前会话保存的AuthUser
+        AuthUser authUser = (AuthUser) japUserStore.get(request, response).getAdditional();
+        AuthToken token = authUser.getToken();
+        return japTemplate.opsForSocial().getUserInfo("gitee", token);
+    }
+
+    @RequestMapping("/gitee/refresh-token")
+    public JapResponse refreshToken(HttpServletRequest request, HttpServletResponse response){
+        // 当前会话保存的AuthUser
+        AuthUser authUser = (AuthUser) japUserStore.get(request, response).getAdditional();
+        AuthToken token = authUser.getToken();
+        // AuthGiteeRequest中没有实现refresh方法，故会报错。
+        return japTemplate.opsForSocial().refreshToken("gitee",token);
+    }
+
+    // 同样AuthGiteeRequest没有实现revoke，故暂不测试revokeToken
 }
